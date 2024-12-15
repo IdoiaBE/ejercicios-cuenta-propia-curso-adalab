6e69a708002fd2a012edf52e56f676f4fbb845bb
IdoiaBE <1992ibe@gmail.com>
2024-12-15 20:41:59 +0100

    menu toggle finished

--- Ejercicio Clase-2.2-eventos/js/main.js
+++ Ejercicio Clase-2.2-eventos/js/main.js
@@ -1,71 +1,16 @@
-const btn = document.querySelector('.js-button');
-const menuNav = document.querySelector('.js-menuNav');
-const darkLink = document.querySelector('.js-dark');
-const ligthLink = document.querySelector('.js-ligth');
-const main = document.querySelector('.js-main');
 
-btn.addEventListener('click', () => {
-  //menuNav.classList.remove('hidden');
-  menuNav.classList.toggle('hidden');
+// Primer paso: Recogo los elementos de HTML que quiero usar o sobre los que quiero actuar (escuchar, cambiar...)
+
+const buttonMenu = document.querySelector('.js-btnMenu')
+const menuList = document.querySelector('.js-listMenu')
+
+// Segundo paso: Escucho el evento y programo una respuesta
+//Sintáxis= nombre-de-la-constante o variable-creada-en-js.addEventListener('tipo de evento', código a ejecutar)
+
+buttonMenu.addEventListener('click', () => {
+  menuList.classList.toggle('hidden');
+  //element.classList.add('class') --> para añadir una clase de css a ese elemento en el HTML
+  //element.classList.remove('class') --> para quitar una clase de css a ese elemento en el HTML
+  //element.classList.togglr('class') --> para alternar una clase de css a ese elemento en el HTML (si no la tiene se la pone y si la tiene se la quita)
 });
 
-darkLink.addEventListener('click', () => {
-  main.classList.add('dark');
-  main.classList.remove('ligth');
-});
-
-ligthLink.addEventListener('click', () => {
-  main.classList.add('ligth');
-  main.classList.remove('dark');
-});
-
-//seccion modal
-const modal = document.querySelector('.js-modal');
-const closeModal = document.querySelector('.js-close');
-
-closeModal.addEventListener('click', () => {
-  modal.classList.add('hidden');
-});
-
-const form = document.querySelector('.js-form');
-const name = document.querySelector('.js-name');
-
-form.addEventListener('submit', (event) => {
-  console.log(event);
-  event.preventDefault();
-  //elemento con la funcion addEventListener
-  console.log(event.currentTarget);
-  //elemento sobre el que hacemos la accion
-  console.log(event.target);
-});
-
-//ejercicio de las frutas
-
-const strawberry = document.querySelector('.fruit-strawberry');
-const banana = document.querySelector('.fruit-banana');
-const kiwi = document.querySelector('.fruit-kiwi');
-
-strawberry.addEventListener('click', (event) => {
-  const selectedFruit = event.currentTarget;
-  selectedFruit.classList.toggle('fruit--selected');
-});
-
-banana.addEventListener('click', (event) => {
-  const selectedFruit = event.currentTarget;
-  selectedFruit.classList.toggle('fruit--selected');
-});
-
-kiwi.addEventListener('click', (event) => {
-  const selectedFruit = event.currentTarget;
-  selectedFruit.classList.toggle('fruit--selected');
-});
-
-const ul = document.querySelector('.fruits');
-ul.addEventListener('click', (event) => {
-  console.log(event);
-  //elemento con la funcion addEventListener
-  console.log(event.currentTarget);
-  //elemento sobre el que hacemos la accion
-  console.log(event.target);
-  // acciones sobre el elemento clickado
-});
