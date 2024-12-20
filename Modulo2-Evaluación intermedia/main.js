6b97b11a21aaa92ac57aae0ce0f8ea48863996d8
IdoiaBE <1992ibe@gmail.com>
2024-12-20 10:20:51 +0100

    select info collected

--- Modulo2-Evaluación intermedia/main.js
+++ Modulo2-Evaluación intermedia/main.js
@@ -6,11 +6,11 @@
 //PASOS
 // 1. Crear el html. --Linkear el js (script)
 // 2. Traer al js todos los elementos que voy a escuchar, modificar...---select, botón, texto
-// 3.Escuchar el evento del click en el botón jugar y programar respuesta
+// 3.Escuchar el evento del click en el botón jugar, recoger datos y programar respuesta
 //    -Crear un función que escuche el evento (addEventListener)
-//    -Crear un función que maneje la respuesta (handleClick)
+//    -Crear un función que maneje la respuesta (handleClick) -> recoger datos select, compararlos y pintar mensaje
 //    -Crear un función que genere un número aleatorio (Math.random)
-//    -Crear un función que compare el número generado con las opciones del select y pinte el texto en el html (Condicional, innerHTML)
+//    -Crear un función que asocie los números aleatorios con las acciones piedra, papel o tijera
 
 'use strict';
 
@@ -20,8 +20,35 @@ const playButton = document.querySelector('.js-btn');
 
 const message = document.querySelector('.js-text');
 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+   }  
+
+// function numberToAction (){
+//     if (getRandomNumber <= 3){
+//         getRandomNumber = piedra
+//     } else (getRandomNumber >= 7) {
+//         getRandomNumber = papel
+//     }
+// }
+   
+
 function handleClick () {
+    const selectValue = selectOptions.value;
     
+    console.log (selectValue)
+    
+    const isRock = selectValue === 'rock';
+
+    console.log (isRock)
+
+    const isPaper = selectValue === 'paper';
+
+    console.log (isPaper)
+
+    const isScissors = selectValue === 'scissors';
+    console.log(isScissors)
+
 }
 
 playButton.addEventListener('click', handleClick)
